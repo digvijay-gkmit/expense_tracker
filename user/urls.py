679a44d84c3fd6bd6df3a182fdd0edf9826a3058
mac <mac@macs-MacBook-Pro-2.local>
2024-12-12 16:10:02 +0530

    feat: added user routes

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("signup/", UserSignupView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
-    path("test/", UserTestView.as_view(), name="test"),
+    # path("test/", UserTestView.as_view(), name="test"),
 ]
