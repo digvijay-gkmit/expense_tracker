c70fd116dcfad97a6463ca9ba6e3bc2a9a33989c
digvijay-gkmit <digvijay@gkmit.co>
2024-12-16 11:39:45 +0530

    added timedelta ,app names ,jwt settings

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
+    path('category/', include('category.urls')),
+    path('transact/', include('transaction.urls')),
 ]
--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser
-
+from category.models import Category 
+PREDEFINED_CATEGORIES = [
+    'Salary',
+    'Food',
+    'Transport',
+    'Entertainment',
+]
 
 class UserSerializer(serializers.ModelSerializer):
     """
@@ -13,8 +19,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = "__all__"
 
+    def validate_username(self, value):
+        if CustomUser.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with this username already exists.")
+        return value
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
+
+        for category_name in PREDEFINED_CATEGORIES:
+            category, created = Category.objects.get_or_create(name=category_name)
+            user.categories.add(category)
+
         return user
 
 
