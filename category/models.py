20d641a61a17af966f3a7fe3e9b6f5a15756b161
digvijay-gkmit <digvijay@gkmit.co>
2024-12-25 13:59:21 +0530

    feat: add slug , inherits base model

--- category/models.py
+++ category/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 from user.models import CustomUser
 import uuid
+from base.models import BaseModel
 
 # Create your models here.
 
-class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Category(BaseModel):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
-        CustomUser, 
-        on_delete=models.CASCADE, 
-        null=True, 
-        blank=True, 
-        related_name='categories',
+        CustomUser,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+        related_name="categories",
     )
-    is_active = models.BooleanField(default=True) # False => Soft-deleted
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)  # False => Soft-deleted
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
